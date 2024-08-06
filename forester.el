dfdde482ecc6d4f88742b2e5c38e201317962ed1
Steven Schaefer <stschaef@umich.edu>
2024-08-06 17:45:07 -0400

    asdf

--- forester.el
+++ forester.el
@@ -6,12 +6,21 @@
   (let
       ((default-directory "~/forest")
        (command "forester new forest.toml --dest=trees --prefix=sss"))
-    (shell-command command))))
+    (shell-command command)))
 
 (define-derived-mode forester-mode fundamental-mode "Forester"
   "Major mode for editing Forester code."
   )
 
+;; Define a setup function for Forester mode
+(defun forester-mode-setup ()
+  "Setup soft line wrap and other settings for Forester mode."
+  (visual-line-mode 1))
+
+;; Add the setup function to the Forester mode hook
+(add-hook 'forester-mode-hook 'forester-mode-setup)
+
+(add-to-list 'auto-mode-alist '("\\.tree\\'" . forester-mode))
 
 (provide 'forester)
 ;;; forest.el ends here
