3a13f2974be695d8220c88c01c83c116e41145a5
Steven Schaefer <stschaef@umich.edu>
2025-02-06 19:32:29 -0500

    2/6

--- server.sh
+++ server.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+# Start the Python server in the background
 python3 -m http.server 1313 -d output &
+# Save the process ID of the Python server
+PYTHON_SERVER_PID=$!
 
+# Run the watch script
 ./watch.sh
+
+# Kill the Python server after the watch script finishes
+kill $PYTHON_SERVER_PID
